e257a4f17ca4793dec1e29768689f736f19caf27
ANdRoId5124 <jkj43483@gmail.com>
2022-03-28 22:44:10 +0200

    Lesson10HM

---
+++ Lesson9HomeWork/src/Ex5.java
@@ -0,0 +1,10 @@
+public class Ex5 {
+    public static void main(String[] args) {
+        String str = "Hello";
+        char symbols[] = str.toCharArray();
+        for(int i = 0; i < symbols.length;i++){
+            System.out.print(symbols[i]);
+            System.out.print(symbols[i]);
+        }
+    }
+}
