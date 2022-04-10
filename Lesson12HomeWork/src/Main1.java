5fd8fc21fb89ad7d225ea34deb5b485e20f657bc
ANdRoId5124 <jkj43483@gmail.com>
2022-04-10 15:57:35 +0200

    Lesson12HM

---
+++ Lesson12HomeWork/src/Main1.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write directory");
+        String path = sc.nextLine();
+        sc.close();
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        BufferedWriter validDoc = new BufferedWriter(new FileWriter("files\\validFiles.txt"));
+        BufferedWriter notValidDoc = new BufferedWriter(new FileWriter("files\\notValidFiles.txt"));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line).append("\n");
+        }
+        reader.close();
+
+        String[] content = stringBuilder.toString().split("\n");
+
+        for(int i = 0; i < content.length; i++){
+            if(content[i].length() == 15 && (content[i].startsWith("docnum") || content[i].startsWith("contract"))){
+                 validDoc.write(content[i] + "\n");
+            } else {
+                notValidDoc.write(content[i]+ " :" + "Wrong length or File don't start with docnum or contract" + "\n");
+            }
+        }
+        validDoc.close();
+        notValidDoc.close();
+
+    }
+}
